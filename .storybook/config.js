6588fb2f02604082549b4653b5afa996d12ffab9
mizchi <miz404@gmail.com>
2017-05-04 02:02:28 +0900

    Add storybook

---
+++ .storybook/config.js
@@ -0,0 +1,7 @@
+/* @flow */
+/* eslint-disable */
+import { configure } from '@kadira/storybook'
+
+configure(() => {
+  require('../src/client/components/atoms/__stories/Icon.stories.js')
+}, module)
---
+++ src/client/components/atoms/__stories/Icon.stories.js
@@ -0,0 +1,10 @@
+/* @flow */
+import React from 'react'
+import { storiesOf, action } from '@kadira/storybook'
+
+import Icon from '../Icon'
+
+storiesOf('Button', module)
+  .add('with text', () => (
+    <Icon name='aaa' onClick={action('clicked')}/>
+  ))
