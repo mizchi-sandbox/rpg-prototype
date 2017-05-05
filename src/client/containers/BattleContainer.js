89cb09c4f7bd6f6c119c825a6b6eaaad032fb1b0
mizchi <miz404@gmail.com>
2017-05-05 10:26:45 +0900

    Refactor

--- src/client/containers/BattleContainer.js
+++ src/client/containers/BattleContainer.js
@@ -2,22 +2,22 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import BattleScene from '../components/organisms/BattleScene'
-import type { State } from '../reducers/battle'
+import type { State as BattleReducerState } from '../reducers/battle'
+import type { State as RootReducerState } from '../reducers'
 import type { BattleAction } from '../actions/battleActions'
 // eslint-disable-next-line
 import type { Dispatcher, Connector } from '@mizchi/redux-helper'
 
-export type BattleContainerProps = State & Dispatcher<BattleAction>
+export type BattleContainerProps = BattleReducerState & Dispatcher<BattleAction>
 
 function BattleContainer(props: BattleContainerProps) {
   return <BattleScene {...props} dispatch={props.dispatch} />
 }
 
-const connector: Connector<
-  {},
-  BattleContainerProps,
-  BattleAction
-> = connect(({ battle }) => {
-  return battle
-})
+const mapStateToProps: RootReducerState => BattleReducerState = root =>
+  root.battle
+
+const connector: Connector<{}, BattleContainerProps, BattleAction> = connect(
+  mapStateToProps
+)
 export default connector(BattleContainer)
