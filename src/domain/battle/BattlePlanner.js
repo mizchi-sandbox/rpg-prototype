1ad2e98bcb9644754e9f8d7fd11371a6878c1cce
mizchi <miz404@gmail.com>
2017-05-09 06:16:07 +0900

    Refactor

--- src/domain/battle/BattlePlanner.js
+++ src/domain/battle/BattlePlanner.js
@@ -2,7 +2,32 @@
 import planDamageOponentSingleSkill from './plans/planSingleDamegeOponentSkill'
 import type { Battler } from './Battler'
 import type { BattleState } from './BattleState'
-import type { Command } from './Command'
+import type { Command, CommandApplicationProgress } from './Command'
+
+type CommandPlan = (next: BattleState) => CommandApplicationProgress
+
+export function createCommandPlan(
+  prevEnv: BattleState,
+  skillId: Symbol,
+  actorId: Symbol,
+  plannedTargetId?: Symbol
+): ?CommandPlan {
+  const actor = prevEnv.battlers.find(b => b.id === actorId)
+  const skill = actor && actor.skills.find(s => s.id === skillId)
+
+  if (actor && skill && skill.data) {
+    switch (skill.data.skillType) {
+      case 'DAMAGE_OPONENT_SINGLE':
+        return planDamageOponentSingleSkill(prevEnv, {
+          actor,
+          skill,
+          plannedTargetId
+        })
+    }
+  }
+  // TODO: Assert or throw
+  return null
+}
 
 export function createCommand(
   prevEnv: BattleState,
@@ -10,25 +35,10 @@ export function createCommand(
   actorId: Symbol,
   plannedTargetId?: Symbol
 ): Command {
-  const actor: ?Battler = prevEnv.battlers.find(b => b.id === actorId)
-  const skill = actor && actor.skills.find(s => s.id === skillId)
-
+  const plan = createCommandPlan(prevEnv, skillId, actorId, plannedTargetId)
   return nextEnv => {
-    // return (env: BattleState) => {
-    if (actor && skill && skill.data) {
-      switch (skill.data.skillType) {
-        case 'DAMAGE_OPONENT_SINGLE':
-          return planDamageOponentSingleSkill(prevEnv, {
-            actor,
-            skill,
-            plannedTargetId
-          })(nextEnv)
-        default:
-          return Object.freeze({
-            state: nextEnv,
-            commandResults: []
-          })
-      }
+    if (plan) {
+      return plan(nextEnv)
     } else {
       return {
         state: nextEnv,
@@ -36,5 +46,4 @@ export function createCommand(
       }
     }
   }
-  // }
 }
